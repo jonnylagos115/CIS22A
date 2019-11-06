4df60985098c2bddbc0a3504f5074b1b246318e6
jonnylagos115 <lagosjonathan1@gmail.com>
2019-11-06 08:46:36 -0800

    Add files via upload

--- ass8.cpp
+++ ass8.cpp
@@ -9,6 +9,8 @@
 #include <ctime>
 using namespace std;
 
+unsigned short user_Playturn(char c);
+
 int main()
 {
 	unsigned short counter, rand_dice1, rand_dice2,
@@ -16,21 +18,16 @@ int main()
 
 	c_tpoints = 0;
 	counter = 0;
-	srand (time(NULL));
 	while (c_tpoints < 100)
 	{
 		turn_tpoints = 0;
 		cout << "This is your turn #" << ++counter << endl;
+		srand (time(NULL));
 		for (unsigned short i = 0; i < 3; i++)
 		{
-			rand_dice1 = rand() % 6 + 1;
-			rand_dice2 = rand() % 6 + 1;
-			sum = rand_dice1 + rand_dice2;
-			if (sum == 7) sum = 0;
-			cout << "* You rolled " << rand_dice1 << " and "
-			<< rand_dice2 << ".  That's " << sum << endl;
-			if (!sum) break;
-			turn_tpoints += sum;
+		    user_Playturn('L', turn_tpoints);
+		    user_Playturn('C', turn_tpoints);
+		    user_Playturn('M', turn_tpoints);
 		}
 		c_tpoints += turn_tpoints;
 		cout << "** You scored " << turn_tpoints << " points for this turn\n"
@@ -38,4 +35,21 @@ int main()
 		if (c_tpoints < 100) cout << endl;
 	}
 	return 0;
+}
+
+unsigned short user_Playturn(char player, unsigned short& turn_tp)
+{
+    unsigned short rd1, rd2;
+    for (unsigned short i = 0; i < 3; i++)
+    {
+        rd1 = rand() % 6 + 1;
+        rd2 = rand() % 6 + 1;
+        sum = rand_dice1 + rand_dice2;
+        if (rd1 == 1 || rd2 == 1) sum = 0;
+        cout << "* You rolled " << rand_dice1 << " and "
+        << rand_dice2 << ".  That's " << sum << endl;
+        if (!sum) break;
+        turn_tpoints += sum;
+    }
+    return (sum);
 }
