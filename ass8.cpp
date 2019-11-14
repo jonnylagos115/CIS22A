f45cccb3b6a03f98725e9d791b4fe4ad5b84616c
jonnylagos115 <lagosjonathan1@gmail.com>
2019-11-13 19:36:14 -0800

    Add files via upload

--- ass8.cpp
+++ ass8.cpp
@@ -6,112 +6,104 @@
 */
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 #include <ctime>
 using namespace std;
 
-void    roll_dice(unsigned short numberOfRolls, unsigned short& sum);
-unsigned short  Larrys_turn(unsigned short ret_sum = 0);
-unsigned short  Curlys_turn(unsigned short ret_sum = 0);
-unsigned short  Moes_turn(unsigned short ret_sum = 0);
+bool		player_turn(char player, unsigned short&);
+bool		isEndofTurn(unsigned short, unsigned short, unsigned short&, unsigned short&);
+void		roll_dice(unsigned short&, unsigned short numberOfRolls = 25);
+void		print_winner(char, unsigned short, unsigned short, unsigned short);
 
 int main()
 {
-	unsigned short counter = 0, sum = 0,
-	check_winner = 0;
+	unsigned short	larrys_tpoints = 0, curlys_tpoints = 0,
+    moes_tpoints = 0;
+	char	winner = 0;
 
-	do
+    srand(time(NULL));
+	while (winner == 0)
 	{
-        check_winner = Larrys_turn();
-        check_winner = Curlys_turn();
-        //check_winner = Moes_turn();
-		if (check_winner == 0) cout << endl;
-		abort();
-	}while (check_winner == 0);
-	switch (check_winner)
-	{
-	    case 1:
-	        cout << "Larry ";
-	        sum = Larrys_turn(1);
-	        break;
-        case 2:
-            cout << "Curly ";
-            sum = Curlys_turn(1);
-            break;
-        case 3:
-            cout << "Moe ";
-            sum = Moes_turn(1);
+		if (player_turn('L', larrys_tpoints) == true)
+			winner = 'L';
+		else if (player_turn('C', curlys_tpoints) == true) 
+			winner = 'C';
+		else if (player_turn('M', moes_tpoints) == true)
+			winner = 'M';
 	}
-	cout << "won the game with " << sum << endl;
+	print_winner(winner, larrys_tpoints, curlys_tpoints, moes_tpoints);
 	return 0;
 }
 
-void    roll_dice(unsigned short numberOfRolls, unsigned short& sum)
+void		roll_dice(unsigned short& tpoints, unsigned short numberOfRolls)
 {
-    unsigned short rd1, rd2;
+    unsigned short rd1, rd2, sumOfTurn = 0;
 
-    srand(time(NULL));
     for (unsigned short i = 0; i < numberOfRolls; i++)
     {
         rd1 = rand() % 6 + 1;
         rd2 = rand() % 6 + 1;
-        if (rd1 == 1 || rd2 == 1)
-        {
-            if (rd1 == 1 && rd2 == 1)
-            {
-                 cout << "SKUNK" << endl;
-                 sum = 0; //reset total point of player
-            }
-            break ;
-        }
-        else
-            sum = rd1 + rd2;
-        cout << "* You rolled " << rd1 << " and "
-        << rd2 << ".  That's " << sum << endl;
+		cout << "    You rolled " << rd1 << " and "
+        << rd2 << ".  That's ";
+        if (isEndofTurn(rd1, rd2, tpoints, sumOfTurn)) break;
+		cout << rd1 + rd2 << endl;
+		sumOfTurn += rd1 + rd2;
+		if ((sumOfTurn + tpoints) >= 100) break;
     }
+	cout << "  That's " << sumOfTurn << " points for your turn" << endl;
+	tpoints += sumOfTurn;
 }
 
-unsigned short Larrys_turn(unsigned short ret_sum)
+bool		isEndofTurn(unsigned short rd1, unsigned short rd2, unsigned short& tpoints, unsigned short& sumOfTurn)
 {
-    static unsigned short c_tpoints;
-
-    if (ret_sum != 0)
-        return c_tpoints;
-    cout << "Larry, its your turn " << endl;
-    roll_dice(1, c_tpoints);
-    cout << "Total points = " << c_tpoints;
-    if (c_tpoints >= 100)
-        return 1;
-    else
-        return 0;
+	if (rd1 == 1 || rd2 == 1)
+    {
+        if (rd1 == 1 && rd2 == 1)
+        {
+            cout << "SKUNK" << endl;
+    		tpoints = 0; //Reset total points on SKUNK
+        }
+		else
+			cout << "0" << endl;
+		sumOfTurn = 0;
+		return (true);
+	}
+	return(false);
 }
 
-unsigned short Curlys_turn(unsigned short ret_sum)
+bool 		player_turn(char player, unsigned short& tpoints)
 {
-    static unsigned short c_tpoints;
-
-    if (ret_sum != 0)
-        return c_tpoints;
-    cout << "Curly, its your turn " << endl;
-    roll_dice(3, c_tpoints);
-    cout << "Total points = " << c_tpoints;
-    if (c_tpoints >= 100)
-        return 2;
-    else
-        return 0;
+	switch (player)
+	{
+		case 'L':
+			cout << "Larry, its your turn" << endl;
+			roll_dice(tpoints, 1);
+			break;
+		case 'C':
+			cout << "Curly, its your turn" << endl;
+			roll_dice(tpoints, 3);
+			break;
+		case 'M':
+			cout << "Moe, its your turn" << endl;
+			roll_dice(tpoints);	//Default argument for numberOfRolls is 25 for Moe, not need for Moe to need more than that in any case
+	}
+	if (tpoints >= 100)
+		return (true);
+	cout << "Total points = " << tpoints << "\n\n";
+	return (false);
 }
 
-unsigned short Moes_turn(unsigned short ret_sum)
+void		print_winner(char winner, unsigned short larrys_tpoints, unsigned short curlys_tpoints, unsigned short moes_tpoints)
 {
-    static unsigned short c_tpoints;
-
-    if (ret_sum != 0)
-        return c_tpoints;
-    cout << "Moe, its your turn " << endl;
-    while (c_tpoints >= 100 || c_tpoints)
-        roll_dice(1, c_tpoints);
-    cout << "Total points = " << c_tpoints;
-    if (c_tpoints >= 100)
-        return 3;
-    else
-        return 0;
+	switch (winner)
+	{
+		case 'L':
+			cout << "Larry won the game with " << larrys_tpoints << " points" << endl;
+			break;
+		case 'C':
+			cout << "Curly won the game with " << curlys_tpoints << " points" << endl;
+			break;
+		case 'M':
+			cout << "Moe won the game with " << moes_tpoints << " points" << endl;
+	}
 }
