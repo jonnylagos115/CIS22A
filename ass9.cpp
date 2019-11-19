87b176622f1978bb31c1527eea3ba6a7e55d04a8
jonnylagos115 <lagosjonathan1@gmail.com>
2019-11-19 10:29:38 -0800

    Add files via upload

--- ass9.cpp
+++ ass9.cpp
@@ -12,13 +12,16 @@
 #include <cmath>
 using namespace std;
 
-void 	        insert_studentData(string);
-int             access_studentData(unsigned short, unsigned short, int data = 0);
+void 	        parse_inputData(int[], string);
+void            parse_calculatedData(int[]);
+char			get_letter_grade(int);
+char		    get_suffix(int grade_percent, int grade);
 
 int main()
 {
 	//const unsigned short max_p = 400;
-	const int       setwidthValues[8] = {2, 5, 5, 5, 4, 6, 5, 2};
+	const int       setwidthValues[8] = {4, 5, 5, 5, 4, 6, 5, 2};
+	int             arrData[20] = {};
 	/*unsigned short	total_p, percent_t, nbr_As,
 	nbr_Bs, nbr_Cs, nbr_Ds, nbr_Fs, nbr_of_students,
 	highest_p, lowest_p;*/
@@ -44,51 +47,98 @@ int main()
 	int index = 0;
 	if (getline(inputFile, line))
 	{
-	    insert_studentData(line);
-	    outputFile << access_studentData(index, 2) << setw(setwidthValues[index]);
-	    index++;
-		for (index; index < 11; index++)
-            outputFile << setw(3) << access_studentData(index, 2);
+	    parse_inputData(arrData, line);
+	    parse_calculatedData(arrData);
+	    for (int i = 0; i < 20; i++)
+            cout << arrData[i] << " " << endl;
+        cin.get();
+	    outputFile << arrData[index++]; //Inserting studentID
+	    outputFile << setw(setwidthValues[0]) << arrData[index];
+		while (index++ < 11)
+            outputFile << setw(3) << arrData[index];
 	}
 	inputFile.close();
 	outputFile.close();
 	return 0;
 }
 
-void 	        insert_studentData(string line)
+void 	       parse_inputData(int arrData[], string line)
 {
-	unsigned short index = 0, i = 0;
+	unsigned short index = -1, i = 0;
 
-	while (index < 14)
+	while (++index < 14)
 	{
 	    i = line.find_first_of(' ');
-        access_studentData(index++, 1, stoi(line.substr(0, i)));
+        arrData[index] = stoi(line.substr(0, i));
         line = line.substr(i); //update string line
         i = line.find_first_not_of(' ');
         line = line.substr(i);
 	}
-    access_studentData(index, 1, stoi(line));
+    arrData[index] = stoi(line);
 }
 
-int      access_studentData(unsigned short index, unsigned short option, int data)
+void            parse_calculatedData(int arrData[])
 {
-    static int  arrData[20];
-    int         ret = 0;
+    const unsigned short max_p = 400;
+    int t_asspoints = 0, lowest_p = 20, index = 0,
+    total_points = 0, percent_t, grade;
 
-    switch(option)
+    while (++index < 12)
     {
-        case 1:
-            arrData[index] = data;
-            break;
-        case 2:
-            ret = arrData[index];
+        t_asspoints += arrData[index];
+        if (lowest_p > arrData[index]) lowest_p = arrData[index];
     }
-    return (ret);
+    t_asspoints -= lowest_p;
+    arrData[index] = t_asspoints;
+    while (++index < 16)
+        total_points += arrData[index];
+    total_points += t_asspoints;
+    arrData[index++] = total_points;
+    percent_t = round((static_cast<double>(total_points) / static_cast<double>(max_p)) * 100.);
+    arrData[index++] = percent_t;
+    grade = get_letter_grade(percent_t);
+    arrData[index] = get_suffix(percent_t, grade);
+}
+
+char			get_letter_grade(int grade_percent)
+{
+	char grade;
+
+	switch (grade_percent / 10){
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+			break;
+		case 7:
+			grade = 'C';
+			break;
+		case 6:
+			grade = 'D';
+			break;
+		default:
+			grade = 'F';
+	}
+	return (grade);
+}
+
+char		    get_suffix(int grade_percent, int grade)
+{
+	int remainder;
+
+	remainder = grade_percent % 10;
+	if ((remainder == 9 || remainder == 8 || grade_percent == 100) && grade != 'F')
+		return '+';
+	else if (remainder == 1 || (remainder == 0 && grade_percent != 100 && grade != 'F'))
+		return '-';
+    return ' ';
 }
 
 short	write_add_otherpoints(string& line, ofstream& outputFile)
 {
-	unsigned short total_p = 0;
+	unsigned short total_p = 0, total_ass;
 
 	outputFile << setw(5) << line.substr(0, 3); // writing midterm points
 	total_p += stoi(line.substr(0, 3)); //adding midterm points to total points
