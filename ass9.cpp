39d755ddf558015688b2d7f813b5da7ca9c2ea9f
jonnylagos115 <lagosjonathan1@gmail.com>
2019-11-19 16:30:55 -0800

    Add files via upload

--- ass9.cpp
+++ ass9.cpp
@@ -19,23 +19,13 @@ char		    get_suffix(int grade_percent, int grade);
 
 int main()
 {
-	//const unsigned short max_p = 400;
-	const int       setwidthValues[8] = {4, 5, 5, 5, 4, 6, 5, 2};
+	const int       setwidthValues[7] = {5, 5, 5, 4, 6, 5, 2};
 	int             arrData[20] = {};
-	/*unsigned short	total_p, percent_t, nbr_As,
-	nbr_Bs, nbr_Cs, nbr_Ds, nbr_Fs, nbr_of_students,
-	highest_p, lowest_p;*/
-	//double			avg_of_total_p, avg_of_percent_t, highest_percent, lowest_percent;
+	int 			index = 0, i;
 	ifstream 		inputFile;
 	ofstream		outputFile;
-	string			line; //, highestStudentID, lowestStudentID, sID;
-	//char			grade;
+	string			line;
 
-	/*nbr_As = nbr_Bs = nbr_Cs = nbr_Ds = nbr_Fs = 0;
-	nbr_of_students = 0;
-	avg_of_total_p = avg_of_percent_t = 0.;
-	highest_p = 0;
-	lowest_p = 400;*/
 	inputFile.open("ass7data.txt");
 	outputFile.open("studentReport.txt");
 	if (inputFile.fail() || outputFile.fail()){
@@ -44,18 +34,28 @@ int main()
 	}
 	outputFile << "Student   -----   Assignment Grades  -----  Ass  Mid  Fin LEx Total  Pct Gr" << endl;
 	outputFile << "--------  -- -- -- -- -- -- -- -- -- -- --  ---  ---  --- --- -----  --- --" << endl;
-	int index = 0;
-	if (getline(inputFile, line))
+	while (getline(inputFile, line))
 	{
 	    parse_inputData(arrData, line);
 	    parse_calculatedData(arrData);
-	    for (int i = 0; i < 20; i++)
-            cout << arrData[i] << " " << endl;
-        cin.get();
-	    outputFile << arrData[index++]; //Inserting studentID
-	    outputFile << setw(setwidthValues[0]) << arrData[index];
-		while (index++ < 11)
-            outputFile << setw(3) << arrData[index];
+		if (!(arrData[0] / 10000000)) outputFile << 0; //Accounted for leading 0 of sID
+		outputFile << arrData[0] << " "; //writing student ID
+	    for (i = 1; i < 18; i++)
+		{
+			if (i >= 1 && i <= 11)
+				outputFile << setw(3) << arrData[i]; //writng all 11 assignments points
+			else if (i >= 12 && i <= 17) //writing assignment total, midterm, final, lex, total points, and percent
+			{
+				if (i == 12) //I swap between assignment total & midterm when writing to outputfile
+					outputFile << setw(setwidthValues[index++]) << arrData[15];
+				if (i == 15) 
+					i++;
+				outputFile << setw(setwidthValues[index++]) << arrData[i];
+			}
+		}
+		outputFile << setw(setwidthValues[index++]) << static_cast<char>(arrData[i++]); //writing letter grade
+		outputFile << static_cast<char>(arrData[i]) << endl; //writing letter grade suffix
+		index = 0;
 	}
 	inputFile.close();
 	outputFile.close();
@@ -68,13 +68,13 @@ void 	       parse_inputData(int arrData[], string line)
 
 	while (++index < 14)
 	{
-	    i = line.find_first_of(' ');
-        arrData[index] = stoi(line.substr(0, i));
+	    i = line.find_first_of(' '); //locate first occurence of ' ', I use that index as the number of digits of the current integer
+        arrData[index] = stoi(line.substr(0, i)); //get substring then convert to integral value
         line = line.substr(i); //update string line
-        i = line.find_first_not_of(' ');
-        line = line.substr(i);
+        i = line.find_first_not_of(' '); //skip over spaces to the first available digit
+        line = line.substr(i); //update string line
 	}
-    arrData[index] = stoi(line);
+    arrData[index] = stoi(line); //the last value to insert from input file
 }
 
 void            parse_calculatedData(int arrData[])
@@ -88,15 +88,18 @@ void            parse_calculatedData(int arrData[])
         t_asspoints += arrData[index];
         if (lowest_p > arrData[index]) lowest_p = arrData[index];
     }
-    t_asspoints -= lowest_p;
-    arrData[index] = t_asspoints;
-    while (++index < 16)
-        total_points += arrData[index];
+    while (index < 15)
+	{
+        total_points += arrData[index++];
+	}
+	t_asspoints -= lowest_p;
     total_points += t_asspoints;
-    arrData[index++] = total_points;
+    arrData[index++] = t_asspoints;
+	arrData[index++] = total_points;
     percent_t = round((static_cast<double>(total_points) / static_cast<double>(max_p)) * 100.);
     arrData[index++] = percent_t;
     grade = get_letter_grade(percent_t);
+	arrData[index++] = grade;
     arrData[index] = get_suffix(percent_t, grade);
 }
 
@@ -134,19 +137,4 @@ char		    get_suffix(int grade_percent, int grade)
 	else if (remainder == 1 || (remainder == 0 && grade_percent != 100 && grade != 'F'))
 		return '-';
     return ' ';
-}
-
-short	write_add_otherpoints(string& line, ofstream& outputFile)
-{
-	unsigned short total_p = 0, total_ass;
-
-	outputFile << setw(5) << line.substr(0, 3); // writing midterm points
-	total_p += stoi(line.substr(0, 3)); //adding midterm points to total points
-	line = line.substr(3); //updating string line
-	outputFile << setw(5) << line.substr(0, 4); //writing finals points
-	total_p += stoi(line.substr(0, 4)); //adding finals points to total points
-	line = line.substr(5); //updating string line
-	outputFile << setw(4) << line.substr(0, 2); //writing LEx points
-	total_p += stoi(line.substr(0, 3)); //adding LEx points to total points
-	return (total_p);
 }
